cd15ba1c11abdd8ebb888704cafaa9a39da76067
Josh Boys <josh@arkahna.io>
2026-05-08 09:12:13 +0000

    anvil v0.6.0-beta

--- Formula/anvil.rb
+++ Formula/anvil.rb
@@ -1,25 +1,25 @@
 class Anvil < Formula
   desc "Anvil CLI — structural governance for AI-assisted development"
   homepage "https://github.com/eddacraft/anvil"
-  version "0.5.1-beta"
+  version "0.6.0-beta"
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/eddacraft/anvil/releases/download/v0.5.1-beta/eddacraft-anvil-aarch64-apple-darwin.tar.xz"
-      sha256 "0ef0588fe749c8d2102791fb500316e03f7d0b0582617a0f24f028a344b50da9"
+      url "https://github.com/eddacraft/anvil/releases/download/v0.6.0-beta/eddacraft-anvil-aarch64-apple-darwin.tar.xz"
+      sha256 "dca3be22cac84bfd7995415eb1c136e36312eea5084a27a3b7d0867373cad186"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/eddacraft/anvil/releases/download/v0.5.1-beta/eddacraft-anvil-x86_64-apple-darwin.tar.xz"
-      sha256 "4e1118102c04293341ff4d62237bd69b83ecc1192ca6cb230ecefc1737f468a5"
+      url "https://github.com/eddacraft/anvil/releases/download/v0.6.0-beta/eddacraft-anvil-x86_64-apple-darwin.tar.xz"
+      sha256 "4ddfdcc69eba90a027a92a831e55a9e8cae259f6f54e94164d5b716a90a46245"
     end
   end
   if OS.linux?
     if Hardware::CPU.arm?
-      url "https://github.com/eddacraft/anvil/releases/download/v0.5.1-beta/eddacraft-anvil-aarch64-unknown-linux-gnu.tar.xz"
-      sha256 "cfcd392fb755eff98002d6a023a3556892e6079e16cc46993505f4639fda052f"
+      url "https://github.com/eddacraft/anvil/releases/download/v0.6.0-beta/eddacraft-anvil-aarch64-unknown-linux-gnu.tar.xz"
+      sha256 "042352e4fd241c069614a70a5f916a89c2801e55891652877cfc7e54dc7bac1e"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/eddacraft/anvil/releases/download/v0.5.1-beta/eddacraft-anvil-x86_64-unknown-linux-gnu.tar.xz"
-      sha256 "3e66c7aa3ef6570f35b0a98bb0a635aaf0c3fcbb348733e5caf5dd0b3aafad22"
+      url "https://github.com/eddacraft/anvil/releases/download/v0.6.0-beta/eddacraft-anvil-x86_64-unknown-linux-gnu.tar.xz"
+      sha256 "d44a87bf7e9cb2851e340ca70c486fbcfeb4ee14f5b84862e020797caa079d93"
     end
   end
   license "LicenseRef-Proprietary"
